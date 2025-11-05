eff40883945d2c5e5b856131f10566ead3ff6d7f
Arekq78 <arekgrabarczyk2@gmail.com>
2025-11-05 10:57:15 +0100

    Diagrams for fun
    Added plotting functionality for far and close extremum functions.

--- z2/main.py
+++ z2/main.py
@@ -1,6 +1,36 @@
-from z2.funkcje import far_extremum_function, close_extremum_function, smith_simulator
+import matplotlib.pyplot as plt
+import numpy as np
+from z2.funkcje import far_extremum_function, close_extremum_function, sa_algorithm
+
+
+def plot_functions():
+    x_far = np.linspace(-150, 150, 1000)
+    y_far = [far_extremum_function(x) for x in x_far]
+
+    plt.figure(figsize=(12, 7))
+    plt.plot(x_far, y_far, label="f(x) - far_extremum")
+    plt.title("Wykres Funkcji 1: far_extremum_function")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    x_close = np.linspace(-1, 2, 1000)
+    y_close = [close_extremum_function(x) for x in x_close]
+    plt.figure(figsize=(12, 7))
+    plt.plot(x_close, y_close, label="f(x) - close_extremum", color='orange')
+    plt.title("Wykres Funkcji 2: close_extremum_function")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    plt.close('all')
+
 
 def main():
+    plot_functions()
+
     print("Wybierz funkcję:")
     print("1 – Przyklad 3)")
     print("2 – Przyklad 4)")
@@ -9,7 +39,7 @@ def main():
     T0 = float(input("Podaj temperaturę początkową T0: "))
     k = float(input("Podaj współczynnik chłodzenia k (np. 0.95): "))
 
-    x, fx = smith_simulator(func_id, M, T0, k)
+    x, fx = sa_algorithm(func_id, M, T0, k,2)
     print(f"Maksimum globalne jest: {x: .3f} = {fx: .3f}")
 
 main()
