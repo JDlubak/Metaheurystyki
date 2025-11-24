73ebe041532960129146b89e89ac006319846262
JDlubak <kuba.dl@wp.pl>
2025-11-24 22:28:30 +0100

    fixed crossing and added config file

--- z3/crossing_mutation.py
+++ z3/crossing_mutation.py
@@ -1,29 +1,39 @@
 import random
 
 
-def single_point_crossing(parent1, parent2):
-    crossing_point = random.randint(1, 25)
-    print(f'Wylosowano: {crossing_point}')
-    first_part = parent1['Name'][:crossing_point]
-    second_part = parent2['Name'][crossing_point:]
-    result = first_part + second_part
-    return result
+def single_point_crossing(parent1: str, parent2: str, n: int) -> tuple[str, str]:
+    p = random.randint(1, n - 1)
+    first_child = parent1[:p] + parent2[p:]
+    second_child = parent2[:p] + parent1[p:]
+    return first_child, second_child
 
 
-def double_point_crossing(parent1, parent2):
-    first_point, second_point = sorted(random.sample(range(1, 26), 2))
-    print(f'Wylosowano: {first_point} {second_point}')
-    first_part = parent1['Name'][:first_point]
-    middle_part = parent2['Name'][first_point:second_point]
-    last_part = parent1['Name'][second_point:]
-    result = first_part + middle_part + last_part
-    return result
+def double_point_crossing(parent1: str, parent2: str, n: int) -> tuple[str, str]:
+    p1, p2 = sorted(random.sample(range(1, n), 2))
+    first_child = parent1[:p1] + parent2[p1:p2] + parent1[p2:]
+    second_child = parent2[:p1] + parent1[p1:p2] + parent2[p2:]
+    return first_child, second_child
 
 
-def mutate_individual(individual: dict, mutation_rate: float) -> None:
-    if random.uniform(0, 1) < mutation_rate:
-        mutation_index = random.randint(0, 25)
-        print(f'Wylosowano: {mutation_index}')
+def mutate_individual(individual: dict, mutation_probability: float) -> None:
+    if random.uniform(0, 1) < mutation_probability:
+        mutation_index = random.randint(0, len(individual['Name']) - 1)
         name = list(individual['Name'])
-        name[mutation_index] = '0' if name[ mutation_index] == '1' else '1'
+        name[mutation_index] = '0' if name[mutation_index] == '1' else '1'
         individual['Name'] = ''.join(name)
+
+
+def cross_algorithm(parent1: str, parent2: str,
+                    crossing_probability: float, crossing_method: str) -> tuple[str, str]:
+    if crossing_method not in ('single', 'double'):
+        raise ValueError(f'Błędna metoda krzyżowania: {crossing_method}. Użyj "single"/"double"')
+    n1, n2 = len(parent1), len(parent2)
+    if n1 != n2:
+        raise ValueError(f'Nieoczekiwany błąd: Rodzice mają różną długość! ({n1}, {n2})')
+    if random.uniform(0, 1) > crossing_probability:
+        return parent1, parent2
+    elif crossing_method == 'single':
+        return single_point_crossing(parent1, parent2, n1)
+    elif crossing_method == 'double':
+        return double_point_crossing(parent1, parent2, n1)
+    raise ValueError("Nieoczekiwany błąd: cross_algorithm nic nie zwrócił!")
--- z3/main.py
+++ z3/main.py
@@ -1,21 +1,13 @@
-import pandas as pd
 from handle_population import create_population
 from selection import selection_algorithm
-
-def load_data() -> dict:
-    data = pd.read_csv('problem plecakowy dane CSV tabulatory.csv',
-                       sep='\t')
-    data = data.rename(columns={'Nazwa': 'Name',
-                                'Waga (kg)': 'Weight',
-                                'Wartość (zł)': 'Value'})
-    data['Weight'] = data['Weight'].str.replace(' ', '').astype(int)
-    data['Value'] = data['Value'].str.replace(' ', '').astype(int)
-    items = data.set_index('Numer').to_dict('index')
-    return items
+from load_operations import load_data, load_config
 
 
 item_list = load_data()
-population = create_population(item_list, 100)
+(cross_probability, mutation_probability, population_size,
+ iterations, crossing_method, selection_method) = load_config()
+
+population = create_population(item_list, population_size)
 selection = selection_algorithm(population, 20, 'roulette')
 for item in selection:
     print(item)
